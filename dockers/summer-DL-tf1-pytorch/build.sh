e857c387854e01b0500d92790f8d6d28d53724a3
Tian Xia <SummerRainET2008@gmail.com>
2020-01-22 17:29:41 -0800

    renamed:    summer-DL-tf2-pytorch/Dockerfile -> Dockerfile.share   renamed:    summer-DL-tf2-pytorch/local_bashrc -> local_bashrc   deleted:    summer-DL-tf1-pytorch/Dockerfile   deleted:    summer-DL-tf1-pytorch/bashrc   modified:   summer-DL-tf1-pytorch/build.sh   deleted:    summer-DL-tf2-pytorch/bashrc   modified:   summer-DL-tf2-pytorch/build.sh

--- dockers/summer-DL-tf1-pytorch/build.sh
+++ dockers/summer-DL-tf1-pytorch/build.sh
@@ -1,1 +1,4 @@
+echo "FROM tensorflow/tensorflow:1.15.0-gpu-py3"  > Dockerfile
+cat ../Dockerfile.share >> Dockerfile
+
 sudo docker build -t summer-dl-tf1-pytorch .
--- dockers/summer-DL-tf2-pytorch/build.sh
+++ dockers/summer-DL-tf2-pytorch/build.sh
@@ -1,1 +1,4 @@
+echo "FROM tensorflow/tensorflow:nightly-gpu-py3"  > Dockerfile
+cat ../Dockerfile.share >> Dockerfile
+
 sudo docker build -t summer-dl-tf2-pytorch .
