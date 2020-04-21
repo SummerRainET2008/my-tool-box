e4fa60f22fbef071db00a24c724ec3bf1b1a4932
Tian Xia <summer.xia1@pactera.com>
2020-04-20 17:08:57 -0700

    modified:   dockers/summer-DL-tf2-pytorch/build.sh

--- dockers/summer-DL-tf2-pytorch/build.sh
+++ dockers/summer-DL-tf2-pytorch/build.sh
@@ -1,5 +1,6 @@
 echo "FROM tensorflow/tensorflow:nightly-gpu-py3"  > Dockerfile
 cat ../Dockerfile.share >> Dockerfile
+echo "RUN python3 -m pip install tensorflow" >> Dockerfile
 
 sudo docker build -t summer-dl-tf2-pytorch .
 rm Dockerfile
