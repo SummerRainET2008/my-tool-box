45b6ce7176626a7a9d14a3a883ed2b6bd154452c
Tian Xia <SummerRainET2008@gmail.com>
2020-04-20 19:19:05 -0700

    move dockers into nlp_team  repo.

--- dockers/summer-DL-tf2-pytorch/build.sh
+++
@@ -1,6 +0,0 @@
-echo "FROM tensorflow/tensorflow:nightly-gpu-py3"  > Dockerfile
-cat ../Dockerfile.share >> Dockerfile
-echo "RUN python3 -m pip install tensorflow" >> Dockerfile
-
-sudo docker build -t summer-dl-tf2-pytorch .
-rm Dockerfile
