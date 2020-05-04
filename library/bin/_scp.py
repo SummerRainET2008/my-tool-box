b3ef00110a06076e253231fb12f21cd03ff805d3
Tian Xia <SummerRainET2008@gmail.com>
2020-05-03 20:13:54 -0700

    modified:   library/bin/_scp.py

--- library/bin/_scp.py
+++ library/bin/_scp.py
@@ -3,20 +3,22 @@
 
 from library.include.server_manager import *
 from pa_nlp import nlp
+from pa_nlp.nlp import Logger
 import optparse
 
 def replace_server(addr):
-  global servers
   if "@" not in addr:
     return addr
 
-  servers = ServerManager.get_instance()
-  server = addr[: addr.index("@")]
-  if server in servers:
-    return addr.replace(server + "@", servers[server] + ":")
+  server_manager = ServerManager.get_instance()
+  server_name = addr[: addr.index("@")]
+  login_name = server_manager.get_login(server_name)
+  if login_name is not None:
+    return addr.replace(server_name + "@", login_name + ":")
+
   return addr
 
-if __name__ == "__main__":
+def main():
   parser = optparse.OptionParser(usage = "cmd dev1@dir1 dir2")
   parser.add_option("--recursive", action="store_true")
   #parser.add_option("-q", "--quiet", action = "store_true", dest = "verbose",
@@ -28,12 +30,14 @@ if __name__ == "__main__":
 
   assert len(args) == 2
 
-  srcDir, tgtDir = replace_server(args[0]), replace_server(args[1])
-  dirOpt = "-r" if srcDir.endswith("/") else ""
+  src_dir, tgt_dir = replace_server(args[0]), replace_server(args[1])
+  cmds = ["scp -oStrictHostKeyChecking=no"]
   if options.recursive:
-    cmd = "scp -r -oStrictHostKeyChecking=no %s %s %s" %(dirOpt, srcDir, tgtDir)
-  else:
-    cmd = "scp %s -oStrictHostKeyChecking=no %s %s" %(dirOpt, srcDir, tgtDir)
-  print(cmd)
-  nlp.execute_cmd(cmd)
+    cmds.append("-r")
+  cmds.append(f"{src_dir} {tgt_dir}")
+
+  nlp.execute_cmd(*cmds)
+
+if __name__ == "__main__":
+  main()
 
