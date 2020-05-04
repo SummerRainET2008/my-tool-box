2903760170b11f1cf8df9b7c17fae26f7de7f647
Tian Xia <SummerRainET2008@gmail.com>
2020-05-03 20:19:21 -0700

    modified:   library/bin/_integence_mood_strength_circle.py   modified:   library/bin/_mount_ios_disk.py   deleted:    library/bin/_setup_git.sh

--- library/bin/_mount_ios_disk.py
+++ library/bin/_mount_ios_disk.py
@@ -4,7 +4,7 @@ from sys import argv
 from os import system
 
 if __name__ == "__main__":
-    assert len(argv) == 3, "cmd isofile mount-place"
-    cmd = '''sudo mount -t iso9660 -o loop "%s" "%s" ''' %(argv[1], argv[2])
-    print "OK, it is mounted!" if system(cmd) == 0 else "failed"
+  assert len(argv) == 3, "cmd isofile mount-place"
+  cmd = '''sudo mount -t iso9660 -o loop "%s" "%s" ''' %(argv[1], argv[2])
+  print "OK, it is mounted!" if system(cmd) == 0 else "failed"
             
--- library/bin/_setup_git.sh
+++
@@ -1,8 +0,0 @@
-git config --global user.name "Summer Xia"
-git config --global user.email "SummerRainET2008@gmail.com"
-git config --global core.editor "vim"
-git config --global diff.tool vimdiff
-
-# >> git credential-osxkeychain
-# >> git config --global credential.helper osxkeychain
-# >> git config credential.helper store       # in ubuntu
