cce7db406b41e4fd2aded00f5bae3690bb691735
Tian Xia <SummerRainET2008@gmail.com>
2020-07-31 03:58:26 -0800

    modified:   settings/configurations/install_docker.sh  	modified:   settings/configurations/install_nvidia_docker_image.sh

--- settings/configurations/install_docker.sh
+++ settings/configurations/install_docker.sh
@@ -5,5 +5,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+export version=5:19.03.12~3-0~ubuntu-bionic;   
+sudo apt-get install docker-ce=$version docker-ce-cli=$version containerd.io
+#sudo apt-get install docker-ce docker-ce-cli containerd.io
 
