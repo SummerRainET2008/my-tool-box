4aaf54c965c85d9c0ea9c6a2affa3f2a6c42d759
Tian Xia <SummerRainET2008@gmail.com>
2020-10-13 23:13:05 -0700

    modified:   library/bin/_install_package.py   modified:   settings/tags.dict

--- library/bin/_install_package.py
+++ library/bin/_install_package.py
@@ -49,10 +49,10 @@ def run_pip(options, args):
     cmd = f"{py} -m pip search {options.search}"
 
   elif options.install is not None:
-    cmd = f"pip-{options.python} install {options.install} --user {options.user}"
+    cmd = f"pip{options.python} install {options.install} --user {options.user}"
 
   elif options.uninstall is not None:
-    cmd = f"sudo pip-{options.python} uninstall {options.uninstall}"
+    cmd = f"sudo pip{options.python} uninstall {options.uninstall}"
 
   elif options.upgrade:
     cmd = f"sudo {py} -m pip install pip --upgrade"
@@ -79,7 +79,7 @@ def main():
   parser.add_option("--uninstall", help="")
   parser.add_option("--update", action="store_true", help="")
   parser.add_option("--upgrade", action="store_true", help="")
-  parser.add_option("--python", default="3.7", help="default 3.7")
+  parser.add_option("--python", default="3", help="default 3")
   parser.add_option("--list_installed", action="store_true", help="")
   parser.add_option("--user", default="summer", help="")
   (options, args) = parser.parse_args()
