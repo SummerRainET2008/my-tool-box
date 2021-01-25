7d12df0d2dd2272cbd7e358702378e39ab52e467
Tian Xia <SummerRainET2008@gmail.com>
2021-01-25 11:11:53 -0800

    new file:   library/bin/_start_dropbox.sh   new file:   library/bin/_start_tensorboard.sh   new file:   library/bin/_update_from_patbert_mine_to_pingan_bert.sh   new file:   library/bin/_update_from_pingan_bert_to_patbert_mine.sh   modified:   settings/bashrc

---
+++ library/bin/_start_dropbox.sh
@@ -0,0 +1,1 @@
+nohup ~/.dropbox-dist/dropboxd &
---
+++ library/bin/_update_from_patbert_mine_to_pingan_bert.sh
@@ -0,0 +1,4 @@
+cd /Users/summer/inf/study/experiments/research_area.nlp/pingan_bert
+rm -r *
+
+cp -r /Users/summer/inf/study/experiments/research_area.nlp/patbert_mine/* .
