23a88a65b2de1bdd1cd79281170cb2aabaaa5129
Tian Xia <SummerRainET2008@gmail.com>
2021-07-10 19:46:52 -0700

    modified:   library/bin/_update_from_pingan_bert_to_patbert_mine.sh   modified:   library/data/servers.pydict   modified:   settings/bashrc

--- library/bin/_update_from_pingan_bert_to_patbert_mine.sh
+++ library/bin/_update_from_pingan_bert_to_patbert_mine.sh
@@ -3,3 +3,7 @@ rm -r *
 
 cp -r /Users/summer/inf/study/experiments/research_area.nlp/pingan_bert/* .
 rm -r palframe
+
+cd /Users/summer/inf/study/experiments/research_area.nlp/PAL_frame
+rm -r palframe
+cp -r /Users/summer/inf/study/experiments/research_area.nlp/pingan_bert/palframe .
